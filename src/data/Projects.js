34827c42cb0434fd3d21fc09285e3921428f1557
Antônio <aclcampos@live.com>
2022-01-07 21:58:35 -0300

    Estilizando...

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -7,6 +7,27 @@ const projects = [
     urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
     urlCode: 'https://github.com/antonioclc/starwars-planet-search',
     printScreen: StarWarsPlanetSearch,
+  },
+  {
+    name: 'StarWars Planet Search',
+    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
+    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
+    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
+    printScreen: StarWarsPlanetSearch,
+  },
+  {
+    name: 'StarWars Planet Search',
+    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
+    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
+    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
+    printScreen: StarWarsPlanetSearch,
+  },
+  {
+    name: 'StarWars Planet Search',
+    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
+    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
+    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
+    printScreen: StarWarsPlanetSearch,
   }
 ];
 
