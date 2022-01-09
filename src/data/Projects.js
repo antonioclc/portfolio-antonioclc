6b9e61bdd0242429f5772ff9d2ed3483d33bab9c
Antônio <aclcampos@live.com>
2022-01-09 01:27:57 -0300

    Adicionando estilização

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,34 +1,21 @@
 import StarWarsPlanetSearch from '../images/screencapture-starwars-planet-seacrh.png'
+import FrontendOnlineStore from '../images/frontend-online-store.png'
 
 const projects = [
   {
     name: 'StarWars Planet Search',
-    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
+    description:'Consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
     urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
     urlCode: 'https://github.com/antonioclc/starwars-planet-search',
     printScreen: StarWarsPlanetSearch,
   },
   {
-    name: 'StarWars Planet Search',
-    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
-    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
-    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
-    printScreen: StarWarsPlanetSearch,
+    name: 'Frontend Online Store',
+    description:'Simula um e-commerce, tendo filtragem por categoria ou digitando no campo de busca, carrinho com as compras adicionadas, avaliação do produto, finalização do pedido, entre outras funcionalidades.',
+    urlPage: 'https://antonioclc.github.io/frontend-online-store/',
+    urlCode: 'https://github.com/antonioclc/frontend-online-store',
+    printScreen: FrontendOnlineStore,
   },
-  {
-    name: 'StarWars Planet Search',
-    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
-    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
-    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
-    printScreen: StarWarsPlanetSearch,
-  },
-  {
-    name: 'StarWars Planet Search',
-    description:'Este projeto consiste em gerar uma tabela com alguns dos planetas do universo StarWars, onde o usuário terá opções para filtrar os planetas de acordo com determinadas características, assim como ordenar de maneira ascendente ou descendente.',
-    urlPage: 'https://antonioclc.github.io/starwars-planet-search/',
-    urlCode: 'https://github.com/antonioclc/starwars-planet-search',
-    printScreen: StarWarsPlanetSearch,
-  }
 ];
 
 export default projects;
