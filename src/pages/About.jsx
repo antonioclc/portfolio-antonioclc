5fa210860d246a07fd5032283b2febb4aacfdebc
Antônio <aclcampos@live.com>
2022-01-12 13:38:16 -0300

    Trocada foto do perfil

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import myPhoto from '../images/minha-foto.jpg'
+import myPhoto from '../images/minha-foto.jpeg'
 
 
 export default function About() {
